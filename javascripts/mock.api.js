388924a18580b85f45caf9fb013c1a6b3a24ac88
Bruce Wang <brucewangsg@icloud.com>
2018-01-07 21:35:21 +0800

    Add icons to dropdown selectors

--- javascripts/mock.api.js
+++ javascripts/mock.api.js
@@ -9,42 +9,52 @@
         callback([{
           id : 1,
           title : "Twitter",
+          img : "/img/icon.twitter.svg",
           description : "Twitter Applets can help you manage and save tweets, keep an eye on #hashtags, and much more."
         }, {
           id : 2,
           title : "Date & Time",
+          img : "/img/icon.date.svg",
           description : "Find the best date and time for your scheduled task."
         }, {
           id : 3,
           title : "RSS Feed",
+          img : "/img/icon.rss.svg",
           description : "This service deal with Really Simple Syndicate."
         }, {
           id : 4,
           title : "SMS",
+          img : "/img/icon.sms.svg",
           description : "Get important notifications on your phone via SMS."
         }, {
           id : 5,
           title : "Email",
+          img : "/img/icon.email.svg",
           description : "Respond to inbound emails for any smtp server."
         }, {
           id : 6,
           title : "Weather",
+          img : "/img/icon.weather.svg",
           description : "Today's weather report."
         }, {
           id : 7,
           title : "Phone Call",
+          img : "/img/icon.phone.svg",
           description : "This service can trigger Applets when you leave a voicemail at the IFTTT number."
         }, {
           id : 8,
           title : "Delicious",
+          img : "/img/icon.delicious.svg",
           description : "Delicious is a social bookmarking web service for storing, sharing, and discovering web bookmarks."
         }, {
           id : 9,
           title : "Facebook",
+          img : "/img/icon.facebook.svg",
           description : "Manage your profile, posting, photos and more with Facebook Applets that work with the world's largest social networking site."
         }, {
           id : 10,
           title : "Tumblr",
+          img : "/img/icon.tumblr.svg",
           description : "Tumblr is a blogging platform that allows users to post text, images, videos, links, quotes, and audio."
         }]);
       } else if (path == "/api/triggers") {
@@ -69,42 +79,52 @@
           callback([{
             id : 1,
             title : "Twitter",
+            img : "/img/icon.twitter.svg",
             description : "Twitter Applets can help you manage and save tweets, keep an eye on #hashtags, and much more."
           }, {
             id : 2,
             title : "Date & Time",
+            img : "/img/icon.date.svg",
             description : "Find the best date and time for your scheduled task."
           }, {
             id : 3,
             title : "RSS Feed",
+            img : "/img/icon.rss.svg",
             description : "This service deal with Really Simple Syndicate."
           }, {
             id : 4,
             title : "SMS",
+            img : "/img/icon.sms.svg",
             description : "Get important notifications on your phone via SMS."
           }, {
             id : 5,
             title : "Email",
+            img : "/img/icon.email.svg",
             description : "Respond to inbound emails for any smtp server."
           }, {
             id : 6,
             title : "Weather",
+            img : "/img/icon.weather.svg",
             description : "Today's weather report."
           }, {
             id : 7,
             title : "Phone Call",
+            img : "/img/icon.phone.svg",
             description : "This service can trigger Applets when you leave a voicemail at the IFTTT number."
           }, {
             id : 8,
             title : "Delicious",
+            img : "/img/icon.delicious.svg",
             description : "Delicious is a social bookmarking web service for storing, sharing, and discovering web bookmarks."
           }, {
             id : 9,
             title : "Facebook",
+            img : "/img/icon.facebook.svg",
             description : "Manage your profile, posting, photos and more with Facebook Applets that work with the world's largest social networking site."
           }, {
             id : 10,
             title : "Tumblr",
+            img : "/img/icon.tumblr.svg",
             description : "Tumblr is a blogging platform that allows users to post text, images, videos, links, quotes, and audio."
           }]);
         } else {
